b6c69dea15d270cc68b80ae720d3d9389dab0be5
FillipeKF <luvpratico@gmail.com>
2026-03-17 22:39:59 -0300

    Change Postgres connection string settings
    Updated database connection string for Postgres.

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const upload = multer({ storage });
 // POSTGRES (NEON)
 // =======================
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_5xEZNaQXO8ye@ep-rapid-pine-acre78eh-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=verify-full",
+  connectionString: "postgresql://neondb_owner:npg_ojkm7E2ZpMHn@ep-rapid-pine-acre78eh-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   ssl: true
 });
 
